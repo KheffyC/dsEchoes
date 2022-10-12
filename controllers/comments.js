e704bd3c9e9b92cc365fa49ce0efb74a141a8b75
KheffyC <kheffy.cervantez@gmail.com>
2022-10-11 18:16:49 -0700

    add 'delete' functionality to replies in thread

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,6 +2,7 @@ const Group = require('../models/group')
 
 module.exports = {
     create,
+    delete: deleteComment,
 }
 
 async function create(req, res){
@@ -17,6 +18,20 @@ async function create(req, res){
                 res.redirect(`/groups/${group.id}`)
             })
         }
+    } catch(err){
+        console.log(err.message)
+    }
+}
+
+async function deleteComment(req, res){
+    let group = await Group.findById(req.params.groupid)
+    try{
+        if(!group) return res.redirect(`/groups/${group._id}`)
+        let post = group.posts.id(req.params.postid)
+        post.comments.remove(req.params.commentsid)
+        group.save(function(err){
+            res.redirect(`/groups/${group._id}`)
+        })
     } catch(err){
         console.log(err.message)
     }
--- routes/comments.js
+++ routes/comments.js
@@ -4,5 +4,6 @@ const commentsCtrl = require('../controllers/comments')
 const isLoggedIn = require('../config/auth')
 
 router.post('/groups/:groupid/posts/:postid/comments', commentsCtrl.create)
+router.delete('/groups/:groupid/posts/:postid/comments/:commentsid', commentsCtrl.delete)
 
 module.exports = router
